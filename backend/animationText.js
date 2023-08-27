966270ea703d136a0fdd33655fea3e34048e3171
Harsh Sharma <harsh464565@gmail.com>
2023-08-27 21:00:02 +0530

    added the homepage

---
+++ backend/animationText.js
@@ -0,0 +1,12 @@
+let typed = new Typed(".auto-type", {
+  strings: [
+    "Convenience Redefined",
+    "Quality Assurance",
+    "Personalized Shopping",
+    "Fast and Reliable Delivery",
+  ],
+  typeSpeed: 50,
+  backSpeed: 50,
+  loop: true
+});
+
