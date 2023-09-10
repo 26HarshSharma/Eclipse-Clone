f5118121ea6923141ee3035e74920bf7ded5ba2d
Harsh Sharma <harsh464565@gmail.com>
2023-09-10 12:01:01 +0530

    asking for confirmation logic implementation

--- backend/productsList.js
+++ backend/productsList.js
@@ -36,8 +36,7 @@ console.log(`Value 1: ${param1}`);
 const receipt = document.getElementById("receipt");
 const finalCostBtn = document.getElementById("final-cost-btn");
 receipt.style.display = "none";
-finalCostBtn.style.display = "none"
-
+finalCostBtn.style.display = "none";
 
 auth.onAuthStateChanged((user) => {
   if (user === null) return;
@@ -81,55 +80,60 @@ auth.onAuthStateChanged((user) => {
     }
   });
 });
-let flag = true;
+
 const totalPrice = document.getElementById("total-btn");
 totalPrice.addEventListener("click", () => {
-  if (flag) {
-    finalCostBtn.style.display = "flex";
-    let price = 0;
-    const prices = document.querySelectorAll(".price-input");
-    for (const element of prices) {
-      price = price + +element.value;
-    }
-    const totalPricePara = document.createElement("p");
-    totalPricePara.textContent = price;
-    totalPricePara.className = "total-price-para";
-    document.getElementById("total-price-container").append(totalPricePara);
-    const discountRateLable = document.createElement("label");
-    const discountRateInput = document.createElement("input");
-    discountRateInput.className = "input-element";
-    discountRateLable.className = "lable-element";
-    discountRateLable.textContent = "Discount Rate (*not mandatory)";
-    document
-      .getElementById("discount-rate-container")
-      .append(discountRateLable);
-    document
-      .getElementById("discount-rate-container")
-      .append(discountRateInput);
-    const deliveryChargesLable = document.createElement("label");
-    const deliveryChargesInput = document.createElement("input");
-    deliveryChargesLable.className = "lable-element";
-    deliveryChargesInput.className = "input-element";
-    deliveryChargesLable.textContent = "Delivery Charges (*not mandatory)";
-    document
-      .getElementById("delivery-charges-container")
-      .append(deliveryChargesLable);
-    document
-      .getElementById("delivery-charges-container")
-      .append(deliveryChargesInput);
-    
-    finalCostBtn.addEventListener("click", () => {
-      const finalCosth3 = document.createElement("h3");
-      const finalCostPara = document.createElement("p");
-      finalCostPara.style.fontSize = "21px";
-      finalCosth3.textContent = "Final Cost Price";
-      console.log(discountRateInput.value / 100);
-      finalCostPara.textContent =
-        (price -
-        (price * (discountRateInput.value / 100))) +
-        (+deliveryChargesInput.value);
-      document.getElementById("final-cost-container").append(finalCostPara);
-    });
-    flag = false;
+  let price = 0;
+  finalCostBtn.style.display = "flex";
+  if(document.querySelectorAll(".total-price-para").length > 0) {
+    document.querySelectorAll(".total-price-para")[0].remove();
   }
+  if(document.querySelectorAll(".input-element").length > 0) {
+    document.querySelectorAll(".input-element")[0].remove();
+  }
+  if(document.querySelectorAll(".lable-element").length > 0) {
+    document.querySelectorAll(".lable-element")[0].remove();
+  }
+  if(document.querySelectorAll("#delivery-charges-container .lable-element").length > 0) {
+    document.querySelectorAll("#delivery-charges-container .lable-element")[0].remove();
+  }
+  if(document.querySelectorAll("#delivery-charges-container .input-element").length > 0) {
+    document.querySelectorAll("#delivery-charges-container .input-element")[0].remove();
+  }
+
+  const prices = document.querySelectorAll(".price-input");
+  for (const element of prices) {
+    price = price + +element.value;
+  }
+  const totalPricePara = document.createElement("p");
+  totalPricePara.textContent = price;
+  totalPricePara.className = "total-price-para";
+  document.getElementById("total-price-container").append(totalPricePara);
+  const discountRateLable = document.createElement("label");
+  const discountRateInput = document.createElement("input");
+  discountRateInput.className = "input-element";
+  discountRateLable.className = "lable-element";
+  discountRateLable.textContent = "Discount Rate (*not mandatory)";
+  document.getElementById("discount-rate-container").append(discountRateLable);
+  document.getElementById("discount-rate-container").append(discountRateInput);
+  const deliveryChargesLable = document.createElement("label");
+  const deliveryChargesInput = document.createElement("input");
+  deliveryChargesLable.className = "lable-element";
+  deliveryChargesInput.className = "input-element";
+  deliveryChargesLable.textContent = "Delivery Charges (*not mandatory)";
+  document.getElementById("delivery-charges-container").append(deliveryChargesLable);
+  document.getElementById("delivery-charges-container").append(deliveryChargesInput);
+  finalCostBtn.addEventListener("click", () => {
+    const finalCostPara = document.createElement("p");
+    if(document.querySelectorAll(".final-cost-para").length > 0) {
+      document.querySelectorAll(".final-cost-para")[0].remove();
+    }
+    finalCostPara.className = "final-cost-para";
+    finalCostPara.style.fontSize = "21px";
+    finalCostPara.textContent =
+      (price -
+      (price * (discountRateInput.value / 100))) +
+      (+deliveryChargesInput.value);
+    document.getElementById("final-cost-container").append(finalCostPara);
+  });
 });
