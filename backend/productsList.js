aefe3ef7cd9b462f6edde00c5166257d16b78cc3
Harsh Sharma <harsh464565@gmail.com>
2023-09-10 09:46:32 +0530

    asking for confirmation logic implementation

--- backend/productsList.js
+++ backend/productsList.js
@@ -31,9 +31,13 @@ function getQueryParameter(name) {
 const param1 = getQueryParameter("param1");
 
 // Use the values as needed
-//console.log(`Value 1: ${param1}`);
+console.log(`Value 1: ${param1}`);
 
-const productsList = document.getElementById("products");
+const receipt = document.getElementById("receipt");
+const finalCostBtn = document.getElementById("final-cost-btn");
+receipt.style.display = "none";
+finalCostBtn.style.display = "none"
+
 
 auth.onAuthStateChanged((user) => {
   if (user === null) return;
@@ -53,21 +57,79 @@ auth.onAuthStateChanged((user) => {
       element.addEventListener("click", function clickHandler() {
         const duplicateElement = element.cloneNode(true);
         element.style.textDecoration = "line-through";
+        receipt.style.display = "flex";
         document.getElementById("available-products").append(duplicateElement);
         const inputElement = document.createElement("input");
+        inputElement.className = "price-input";
         const close = document.createElement("div");
         close.className = "closeBtn";
-        close.innerHTML = `<span class="material-symbols-outlined"> close </span>`;
+        close.innerHTML = `<span class="material-symbols-outlined close"> close </span>`;
         inputElement.placeholder = "Price";
         duplicateElement.append(inputElement);
         duplicateElement.append(close);
         close.addEventListener("click", function closeHandler() {
-          duplicateElement.style.display = "none";
+          duplicateElement.remove();
+          const duplicateElementCount = document.querySelectorAll(".close");
+          if (duplicateElementCount.length === 0) {
+            receipt.style.display = "none";
+          }
           element.style.textDecoration = "none";
-          element.addEventListener("click",clickHandler);
+          element.addEventListener("click", clickHandler);
         });
         element.removeEventListener("click", clickHandler);
       });
     }
   });
+});
+let flag = true;
+const totalPrice = document.getElementById("total-btn");
+totalPrice.addEventListener("click", () => {
+  if (flag) {
+    finalCostBtn.style.display = "flex";
+    let price = 0;
+    const prices = document.querySelectorAll(".price-input");
+    for (const element of prices) {
+      price = price + +element.value;
+    }
+    const totalPricePara = document.createElement("p");
+    totalPricePara.textContent = price;
+    totalPricePara.className = "total-price-para";
+    document.getElementById("total-price-container").append(totalPricePara);
+    const discountRateLable = document.createElement("label");
+    const discountRateInput = document.createElement("input");
+    discountRateInput.className = "input-element";
+    discountRateLable.className = "lable-element";
+    discountRateLable.textContent = "Discount Rate (*not mandatory)";
+    document
+      .getElementById("discount-rate-container")
+      .append(discountRateLable);
+    document
+      .getElementById("discount-rate-container")
+      .append(discountRateInput);
+    const deliveryChargesLable = document.createElement("label");
+    const deliveryChargesInput = document.createElement("input");
+    deliveryChargesLable.className = "lable-element";
+    deliveryChargesInput.className = "input-element";
+    deliveryChargesLable.textContent = "Delivery Charges (*not mandatory)";
+    document
+      .getElementById("delivery-charges-container")
+      .append(deliveryChargesLable);
+    document
+      .getElementById("delivery-charges-container")
+      .append(deliveryChargesInput);
+    
+    finalCostBtn.addEventListener("click", () => {
+      const finalCosth3 = document.createElement("h3");
+      const finalCostPara = document.createElement("p");
+      finalCostPara.style.fontSize = "21px";
+      finalCosth3.textContent = "Final Cost Price";
+      console.log(discountRateInput.value / 100);
+      finalCostPara.textContent =
+        (price -
+        (price * (discountRateInput.value / 100))) +
+        (+deliveryChargesInput.value);
+      document.getElementById("final-cost-container").append(finalCostPara);
+    });
+    flag = false;
+  }
 });
